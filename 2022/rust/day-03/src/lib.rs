fce72eb180865a7ad0e13d8fb36eeb1e06a9d9b4
Andreas K <hamburger1984@gmail.com>
2022-12-03 17:30:59 +0100

    Day 3: part 2 (needs rust nightly.. there's got to be a better way)

--- 2022/rust/day-03/src/lib.rs
+++ 2022/rust/day-03/src/lib.rs
@@ -1,4 +1,6 @@
-use std::collections::HashSet;
+#![feature(iter_next_chunk)]
+
+use std::{collections::HashSet, process::exit};
 
 pub fn process_part1(input: &str) -> String {
     let upper_a = b'A';
@@ -29,22 +31,55 @@ pub fn process_part1(input: &str) -> String {
     return error_sum.to_string();
 }
 
+pub fn process_part2(input: &str) -> String {
+    let upper_a = b'A';
+    let lower_a = b'a';
+
+    let mut lines = input.lines();
+    let mut sum:u64 = 0;
+
+    while let Ok(group) = lines.next_chunk::<3>() {
+        let [a, b, c] = group.map(|line| {
+            return line.bytes().collect::<HashSet<_>>();
+        });
+
+        let mut group_sticker_candidates = a.intersection(&b).filter(|i| c.contains(i));
+        if let Some(group_sticker) = group_sticker_candidates.next() {
+            // if we were serious about this, we would verify, group_sticker_candidates is empty here.
+
+            if *group_sticker > lower_a {
+                sum = sum + (*group_sticker - lower_a) as u64 + 1
+            } else {
+                sum = sum + (*group_sticker - upper_a) as u64 + 27
+            }
+        } else {
+            exit(1);
+        }
+    }
+
+    return sum.to_string();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const TESTINPUT: &str = "
-vJrwpWtwJgWrhcsFMMfFFhFp
+    const TESTINPUT: &str = "vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw
-";
+CrZsJsPPZsGzwwsLwLmpwMDw";
 
     #[test]
     fn test_part1() {
         let result = process_part1(TESTINPUT);
         assert_eq!(result, "157");
+    }
+
+    #[test]
+    fn test_part2() {
+        let result = process_part2(TESTINPUT);
+        assert_eq!(result, "70");
     }
 }
