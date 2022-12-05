ffeab55df0767a555d256a486dc270943eaec9dd
Andreas K <hamburger1984@gmail.com>
2022-12-05 17:30:24 +0100

    Day 5: part 2

--- 2022/rust/day-05/src/lib.rs
+++ 2022/rust/day-05/src/lib.rs
@@ -10,18 +10,11 @@ pub fn process_part1(input: &str) -> String {
     while let Some(stack_line) = stack_lines.next() {
         let stack_bytes = stack_line.bytes().collect::<Vec<u8>>();
 
-        let mut s = 0;
-        loop {
+        for s in 0..stack_count {
             let ci = s * 4 + 1;
-
             let c = stack_bytes[ci];
             if c != 32 {
                 stacks[s].push(c);
-            }
-
-            s = s + 1;
-            if s >= stack_count {
-                break;
             }
         }
     }
@@ -47,6 +40,54 @@ pub fn process_part1(input: &str) -> String {
         }).collect::<Vec<u8>>()).unwrap();
 }
 
+pub fn process_part2(input:&str) -> String {
+    let (stacks_input, operations) = input.split_once("\n\n").unwrap();
+
+    let mut stack_lines = stacks_input.lines().rev();
+
+    let stack_count = stack_lines.next().unwrap().split("   ").count();
+
+    let mut stacks: Vec<Vec<u8>> = vec![Vec::new(); stack_count];
+
+    while let Some(stack_line) = stack_lines.next() {
+        let stack_bytes = stack_line.bytes().collect::<Vec<u8>>();
+
+        for s in 0..stack_count {
+            let ci = s * 4 + 1;
+            let c = stack_bytes[ci];
+            if c != 32 {
+                stacks[s].push(c);
+            }
+        }
+    }
+
+    for op in operations.lines() {
+        let op_parts = op.split(" ").collect::<Vec<&str>>();
+        let (qty, a, b) = (
+            op_parts[1].parse::<u8>().unwrap(),
+            op_parts[3].parse::<usize>().unwrap(),
+            op_parts[5].parse::<usize>().unwrap(),
+        );
+
+        let mut moving = Vec::new();
+
+        for _ in 0..qty {
+            let to_move = &stacks[a - 1].pop().unwrap();
+            moving.push(*to_move);
+        }
+
+        moving.reverse();
+
+        stacks[b-1].append(& mut moving);
+    }
+
+    return String::from_utf8(stacks
+        .iter()
+        .map(|stack| {
+            return *stack.last().unwrap();
+        }).collect::<Vec<u8>>()).unwrap();
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -55,6 +96,12 @@ mod tests {
     fn test_part1() {
         let result = process_part1(TESTINPUT);
         assert_eq!(result, "CMZ");
+    }
+
+    #[test]
+    fn test_part2() {
+        let result = process_part2(TESTINPUT);
+        assert_eq!(result, "MCD");
     }
 
     const TESTINPUT: &str = "    [D]    
