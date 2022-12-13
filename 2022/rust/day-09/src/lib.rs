92823bbf4aa63218887a94b97089f8ecff3389ec
Andreas K <hamburger1984@gmail.com>
2022-12-13 22:52:30 +0100

    Day 9: part 1

--- 2022/rust/day-09/src/lib.rs
+++ 2022/rust/day-09/src/lib.rs
@@ -3,43 +3,82 @@ use std::{collections::HashSet, process::exit};
 pub fn process_part1(input: &str) -> String {
     let mut visited = HashSet::new();
 
-    let (mut tx, mut ty, mut hx, mut hy) = (0, 0, 0, 0);
+    let (mut tx, mut ty, mut hx, mut hy) = (50, 50, 50, 50);
 
     visited.insert((tx, ty));
 
     for line in input.lines() {
+        print!(" > head at {hx},{hy}, tail at {tx},{ty}\n");
         match line.split(' ').collect::<Vec<&str>>().as_slice() {
             ["L", steps] => {
                 print!("Go {steps} left\n");
                 for _ in 0..steps.parse().unwrap() {
                     hx -= 1;
+                    (tx, ty) = calculate_tail(hx, hy, tx, ty);
+                    visited.insert((tx, ty));
                 }
             }
             ["R", steps] => {
                 print!("Go {steps} right\n");
                 for _ in 0..steps.parse().unwrap() {
                     hx += 1;
+                    (tx, ty) = calculate_tail(hx, hy, tx, ty);
+                    visited.insert((tx, ty));
                 }
             }
             ["U", steps] => {
                 print!("Go {steps} up\n");
                 for _ in 0..steps.parse().unwrap() {
                     hy -= 1;
+                    (tx, ty) = calculate_tail(hx, hy, tx, ty);
+                    visited.insert((tx, ty));
                 }
             }
             ["D", steps] => {
                 print!("Go {steps} down\n");
                 for _ in 0..steps.parse().unwrap() {
                     hy += 1;
+                    (tx, ty) = calculate_tail(hx, hy, tx, ty);
+                    visited.insert((tx, ty));
                 }
             }
             _ => {
                 exit(1);
             }
         }
-        print!(" > head at {hx},{hy}\n")
     }
-    return 1.to_string();
+
+    print!(" > head at {hx},{hy}, tail at {tx},{ty}\n");
+
+    return visited.len().to_string();
+}
+
+fn calculate_tail(hx: i32, hy: i32, tx: i32, ty: i32) -> (i32, i32) {
+    if hx == tx && hy == ty {
+        return (tx, ty);
+    }
+
+    if (hx - tx).abs() > 1 || (hy - ty).abs() > 1 {
+        let mut tx2 = tx;
+        let mut ty2 = ty;
+        if hx > tx {
+            tx2 += 1;
+        }
+        if hx < tx {
+            tx2 -= 1;
+        }
+
+        if hy > ty {
+            ty2 += 1;
+        }
+        if hy < ty {
+            ty2 -= 1;
+        }
+
+        return (tx2, ty2);
+    }
+
+    return (tx, ty);
 }
 
 #[cfg(test)]
