3091e5c611aa4e4f09fc975fbc04d45ca7dbb457
Andreas K <hamburger1984@gmail.com>
2022-12-13 23:22:21 +0100

    Day 9: part 2

--- 2022/rust/day-09/src/lib.rs
+++ 2022/rust/day-09/src/lib.rs
@@ -14,7 +14,7 @@ pub fn process_part1(input: &str) -> String {
                 print!("Go {steps} left\n");
                 for _ in 0..steps.parse().unwrap() {
                     hx -= 1;
-                    (tx, ty) = calculate_tail(hx, hy, tx, ty);
+                    (tx, ty) = calculate_tail((hx, hy), (tx, ty));
                     visited.insert((tx, ty));
                 }
             }
@@ -22,7 +22,7 @@ pub fn process_part1(input: &str) -> String {
                 print!("Go {steps} right\n");
                 for _ in 0..steps.parse().unwrap() {
                     hx += 1;
-                    (tx, ty) = calculate_tail(hx, hy, tx, ty);
+                    (tx, ty) = calculate_tail((hx, hy), (tx, ty));
                     visited.insert((tx, ty));
                 }
             }
@@ -30,7 +30,7 @@ pub fn process_part1(input: &str) -> String {
                 print!("Go {steps} up\n");
                 for _ in 0..steps.parse().unwrap() {
                     hy -= 1;
-                    (tx, ty) = calculate_tail(hx, hy, tx, ty);
+                    (tx, ty) = calculate_tail((hx, hy), (tx, ty));
                     visited.insert((tx, ty));
                 }
             }
@@ -38,7 +38,7 @@ pub fn process_part1(input: &str) -> String {
                 print!("Go {steps} down\n");
                 for _ in 0..steps.parse().unwrap() {
                     hy += 1;
-                    (tx, ty) = calculate_tail(hx, hy, tx, ty);
+                    (tx, ty) = calculate_tail((hx, hy), (tx, ty));
                     visited.insert((tx, ty));
                 }
             }
@@ -53,32 +53,99 @@ pub fn process_part1(input: &str) -> String {
     return visited.len().to_string();
 }
 
-fn calculate_tail(hx: i32, hy: i32, tx: i32, ty: i32) -> (i32, i32) {
-    if hx == tx && hy == ty {
-        return (tx, ty);
+pub fn process_part2(input: &str) -> String {
+    let mut visited = HashSet::new();
+
+    let mut rope = vec![(50, 50); 10];
+
+    visited.insert(rope[0]);
+
+    for line in input.lines() {
+        print!(
+            " > head at {:?}, tail at {:?}\n",
+            rope.first().unwrap(),
+            rope.last().unwrap()
+        );
+        match line.split(' ').collect::<Vec<&str>>().as_slice() {
+            ["L", steps] => {
+                print!("Go {steps} left\n");
+                for _ in 0..steps.parse().unwrap() {
+                    rope[0] = (rope[0].0 - 1, rope[0].1);
+                    update_rope(& mut rope);
+                    visited.insert(*rope.last().unwrap());
+                }
+            }
+            ["R", steps] => {
+                print!("Go {steps} right\n");
+                for _ in 0..steps.parse().unwrap() {
+                    rope[0] = (rope[0].0 + 1, rope[0].1);
+                    update_rope(& mut rope);
+                    visited.insert(*rope.last().unwrap());
+                }
+            }
+            ["U", steps] => {
+                print!("Go {steps} up\n");
+                for _ in 0..steps.parse().unwrap() {
+                    rope[0] = (rope[0].0, rope[0].1 - 1);
+                    update_rope(& mut rope);
+                    visited.insert(*rope.last().unwrap());
+                }
+            }
+            ["D", steps] => {
+                print!("Go {steps} down\n");
+                for _ in 0..steps.parse().unwrap() {
+                    rope[0] = (rope[0].0, rope[0].1 + 1);
+                    update_rope(& mut rope);
+                    visited.insert(*rope.last().unwrap());
+                }
+            }
+            _ => {
+                exit(1);
+            }
+        }
     }
 
-    if (hx - tx).abs() > 1 || (hy - ty).abs() > 1 {
-        let mut tx2 = tx;
-        let mut ty2 = ty;
-        if hx > tx {
+    print!(
+        " > head at {:?}, tail at {:?}\n",
+        rope.first().unwrap(),
+        rope.last().unwrap()
+    );
+
+    return visited.len().to_string();
+}
+
+fn update_rope(rope: &mut Vec<(i32, i32)>) {
+    for i in 1..rope.len() {
+        rope[i] = calculate_tail(rope[i - 1], rope[i]);
+    }
+}
+
+fn calculate_tail(h: (i32, i32), t: (i32, i32)) -> (i32, i32) {
+    if h == t {
+        return t;
+    }
+
+    if (h.0 - t.0).abs() > 1 || (h.1 - t.1).abs() > 1 {
+        let mut tx2 = t.0;
+        let mut ty2 = t.1;
+        if h.0 > t.0 {
             tx2 += 1;
         }
-        if hx < tx {
+        if h.0 < t.0 {
             tx2 -= 1;
         }
 
-        if hy > ty {
+        if h.1 > t.1 {
             ty2 += 1;
         }
-        if hy < ty {
+        if h.1 < t.1 {
             ty2 -= 1;
         }
 
         return (tx2, ty2);
     }
 
-    return (tx, ty);
+    return t;
 }
 
 #[cfg(test)]
@@ -91,6 +158,12 @@ mod tests {
         assert_eq!(result, "13");
     }
 
+    #[test]
+    fn test_part2() {
+        let result = process_part2(TEST_INPUT2);
+        assert_eq!(result, "36");
+    }
+
     const TEST_INPUT: &str = "R 4
 U 4
 L 3
@@ -99,4 +172,13 @@ R 4
 D 1
 L 5
 R 2";
+
+    const TEST_INPUT2: &str = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20";
 }
