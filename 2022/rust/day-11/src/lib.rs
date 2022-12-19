b2acbcf7f9dc4d3b03dc2f6b8b50a724486dc223
Andreas K <hamburger1984@gmail.com>
2022-12-19 16:36:07 +0100

    Day 11: part 1

--- 2022/rust/day-11/src/lib.rs
+++ 2022/rust/day-11/src/lib.rs
@@ -4,36 +4,116 @@ pub fn process_part1(input: &str) -> String {
     let mut monkeys: Vec<Monkey> = Vec::new();
 
     for monkey_str in input.split("\n\n") {
+        let mut items = VecDeque::new();
+        let mut opt_operation: Option<(String, String)> = None;
+        let mut opt_divisor: Option<u64> = None;
+        let mut opt_target_true: Option<usize> = None;
+        let mut opt_target_false: Option<usize> = None;
+
         for line in monkey_str.lines().skip(1) {
             match line.trim().split(" ").collect::<Vec<&str>>().as_slice() {
-                ["Operation:", "new", "=", op1, operand, op2] => {
-                    print!("OP      {}{}{}\n", op1, operand, op2);
+                ["Operation:", "new", "=", "old", op1, op2] => {
+                    //print!("OP      {}{}\n", op1, op2);
+                    opt_operation = Some((op1.to_string(), op2.to_string()));
                 }
                 ["Test:", "divisible", "by", test_value] => {
-                    print!("TS      >> {}\n", test_value);
+                    //print!("TS      >> {}\n", test_value);
+                    opt_divisor = Some(test_value.parse().unwrap());
                 }
                 ["If", "true:", "throw", "to", "monkey", target] => {
-                    print!("YES     >> {}\n", target);
+                    //print!("YES     >> {}\n", target);
+                    opt_target_true = Some(target.parse().unwrap());
                 }
                 ["If", "false:", "throw", "to", "monkey", target] => {
-                    print!("NO     >> {}\n", target);
-                }
-                ["Starting", "items:", items] => {
-                    print!("ITEMS   {}\n", items);
+                    //print!("NO     >> {}\n", target);
+                    opt_target_false = Some(target.parse().unwrap());
                 }
                 other => {
-                    print!(" -- {:?}\n", other);
+                    //print!("Items {:?}\n", other);
+                    for item in other.into_iter().skip(2) {
+                        items.push_back(item.trim_end_matches(',').parse().unwrap());
+                    }
+                }
+            }
+        }
+
+        if let Some(operation) = opt_operation {
+            if let Some(divisor) = opt_divisor {
+                if let Some(target_true) = opt_target_true {
+                    if let Some(target_false) = opt_target_false {
+                        monkeys.push(Monkey {
+                            items,
+                            operation,
+                            divisor,
+                            target_true,
+                            target_false,
+                            activities: 0,
+                        });
+                        continue;
+                    }
+                }
+            }
+        }
+        panic!("Incomplete Monkey!");
+    }
+
+    let rounds = 20;
+    let monkey_count = monkeys.len();
+
+    for round in 1..=rounds {
+        print!("Round {}\n", round);
+
+        for i_monkey in 0..monkey_count {
+            //print!("Monkey {}\n", i_monkey);
+
+            let (left, right) = monkeys.split_at_mut(i_monkey);
+            while !right[0].items.is_empty() {
+                if let Some(item) = right[0].items.pop_front() {
+                    //print!(" Inspect {}\n", item);
+                    let new_level = apply_operation(item, &right[0].operation);
+                    //print!("  New level {}\n", new_level);
+
+                    let target_monkey = match new_level % right[0].divisor == 0 {
+                        true => right[0].target_true,
+                        false => right[0].target_false
+                    };
+
+                    //print!("  Throw to {}\n", target_monkey);
+                    if target_monkey > i_monkey {
+                        right[target_monkey-i_monkey].items.push_back(new_level);
+                    } else {
+                        left[target_monkey].items.push_back(new_level);
+                    }
+
+                    right[0].activities += 1;
                 }
             }
         }
     }
 
-    return 1.to_string();
+    let mut activities:Vec<u64> = monkeys.iter().map(|m| {m.activities}).collect();
+    activities.sort_by(|a, b|{b.cmp(a)});
+
+    return (activities[0] * activities[1]).to_string();
 }
 
+fn apply_operation(level: u64, operation: &(String, String)) -> u64 {
+    let b = match operation.1.as_str() {
+        "old" => level,
+        number => number.parse().unwrap(),
+    };
+
+    return match operation.0.as_str() {
+        "*" => (level * b) / 3,
+        "+" => (level + b) / 3,
+        _ => panic!("Unknown operation"),
+    };
+}
+
+#[derive(Debug)]
 struct Monkey {
     items: VecDeque<u64>,
-    operation: (String, String, String),
+    operation: (String, String),
     divisor: u64,
     target_true: usize,
     target_false: usize,
