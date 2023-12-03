d3047950caf6e620d943ff977fb0afc6eb4f3aa5
Andreas K <hamburger1984@gmail.com>
2023-12-03 05:19:39 +0100

    day-01 part 2 => still wrong result

--- 2023/rust/day-01/src/part2.rs
+++ 2023/rust/day-01/src/part2.rs
@@ -1,10 +1,360 @@
 use crate::custom_error::AocError;
 
+enum State {
+    Start,
+    // eight
+    E,
+    EI,
+    EIG,
+    EIGH,
+    // four, five
+    F,
+    FO,
+    FOU,
+    FI,
+    FIV,
+    // nine
+    N,
+    NI,
+    NIN,
+    // one,
+    O,
+    ON,
+    // six, seven
+    S,
+    SI,
+    SE,
+    SEV,
+    SEVE,
+    // two, three
+    T,
+    TW,
+    TH,
+    THR,
+    THRE,
+}
+
 #[tracing::instrument]
 pub fn process(
-    _input: &str,
+    input: &str,
 ) -> miette::Result<String, AocError> {
-    todo!("day 01 - part 1");
+    let mut total = 0;
+    for line in input.lines() {
+        //print!("{} => ", line);
+        let mut first_digit = '-';
+        let mut last_digit = '-';
+        let mut state = State::Start;
+
+        for c in line.chars() {
+            if c.is_digit(10) && c != '0' {
+                //print!("{} ", c);
+                state = State::Start;
+                if first_digit == '-' { first_digit = c; }
+                last_digit = c;
+            } else {
+                state = match state {
+                    State::Start => match c {
+                        'e' => State::E,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::E => match c {
+                        'i' => State::EI,
+                        'e' => State::SE,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::EI => match c {
+                        'g' => State::EIG,
+                        'e' => State::SE,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::EIG => match c {
+                        'h' => State::EIGH,
+                        'e' => State::SE,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::EIGH => match c {
+                        't' => {
+                            //print!("eight ");
+                            if first_digit == '-' { first_digit = '8'; }
+                            last_digit = '8';
+                            State::T
+                        }
+                        'e' => State::SE,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        _ => State::Start
+                    },
+                    State::F => match c {
+                        'i' => State::FI,
+                        'o' => State::FO,
+                        'e' => State::SE,
+                        'f' => State::F,
+                        'n' => State::N,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::FI => match c {
+                        'v' => State::FIV,
+                        'e' => State::SE,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::FIV => match c {
+                        'e' => {
+                            //print!("five ");
+                            if first_digit == '-' { first_digit = '5'; }
+                            last_digit = '5';
+                            State::E
+                        }
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::FO => match c {
+                        'u' => State::FOU,
+                        'e' => State::SE,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::FOU => match c {
+                        'r' => {
+                            //print!("four ");
+                            if first_digit == '-' { first_digit = '4'; }
+                            last_digit = '4';
+                            State::Start
+                        }
+                        'e' => State::SE,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::N => match c {
+                        'i' => State::NI,
+                        'e' => State::SE,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::NI => match c {
+                        'n' => State::NIN,
+                        'e' => State::SE,
+                        'f' => State::F,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::NIN => match c {
+                        'e' => {
+                            //print!("nine ");
+                            if first_digit == '-' { first_digit = '9'; }
+                            last_digit = '9';
+                            State::E
+                        }
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::O => match c {
+                        'n' => State::ON,
+                        'e' => State::SE,
+                        'f' => State::F,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::ON => match c {
+                        'e' => {
+                            //print!("one ");
+                            if first_digit == '-' { first_digit = '1'; }
+                            last_digit = '1';
+                            State::E
+                        }
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::S => match c {
+                        'i' => State::SI,
+                        'e' => State::SE,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::SI => match c {
+                        'x' => {
+                            //print!("six ");
+                            if first_digit == '-' { first_digit = '6'; }
+                            last_digit = '6';
+                            State::Start
+                        }
+                        'e' => State::E,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::SE => match c {
+                        'v' => State::SEV,
+                        'i' => State::EI,
+                        'e' => State::E,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::SEV => match c {
+                        'e' => State::SEVE,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::SEVE => match c {
+                        'n' => {
+                            //print!("seven ");
+                            if first_digit == '-' { first_digit = '7'; }
+                            last_digit = '7';
+                            State::N
+                        }
+                        'i' => State::EI,
+                        'e' => State::E,
+                        'f' => State::F,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::T => match c {
+                        'h' => {
+                            State::TH
+                        }
+                        'w' => {
+                            State::TW
+                        }
+                        'e' => State::E,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::TH => match c {
+                        'r' => {
+                            State::THR
+                        }
+                        'e' => State::E,
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::THR => match c {
+                        'e' => {
+                            State::THRE
+                        }
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::THRE => match c {
+                        'e' => {
+                            //print!("three ");
+                            if first_digit == '-' { first_digit = '3'; }
+                            last_digit = '3';
+                            State::E
+                        }
+                        'f' => State::F,
+                        'n' => State::N,
+                        'o' => State::O,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    },
+                    State::TW => match c {
+                        'o' => {
+                            //print!("two ");
+                            if first_digit == '-' { first_digit = '2'; }
+                            last_digit = '2';
+                            State::O
+                        }
+                        'e' => State::E,
+                        'f' => State::F,
+                        'n' => State::N,
+                        's' => State::S,
+                        't' => State::T,
+                        _ => State::Start
+                    }
+                };
+            }
+        }
+
+        //println!("{}{}", &first_digit, &last_digit);
+        total += first_digit.to_digit(10).unwrap() * 10 + last_digit.to_digit(10).unwrap();
+    }
+
+    return Result::Ok(total.to_string());
 }
 
 #[cfg(test)]
@@ -13,9 +363,14 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        assert_eq!("281", process(input)?);
         Ok(())
     }
 }
