2a9349fb7b310a3679b81ee7a8e38db1e628330b
Andreas K <hamburger1984@gmail.com>
2023-12-03 01:43:41 +0100

    day-01 part 1 => 54877

---
+++ 2023/rust/day-01/src/part1.rs
@@ -0,0 +1,40 @@
+use crate::custom_error::AocError;
+
+#[tracing::instrument]
+pub fn process(
+    input: &str,
+) -> miette::Result<String, AocError> {
+    let mut total = 0;
+    for line in input.lines() {
+        let mut first_digit = '-';
+        let mut last_digit = '-';
+
+        for c in line.chars() {
+            if c.is_digit(10) {
+                if first_digit == '-' {
+                    first_digit = c;
+                }
+                last_digit = c;
+            }
+        }
+
+        total += first_digit.to_digit(10).unwrap() * 10 + last_digit.to_digit(10).unwrap();
+    }
+
+    return Result::Ok(total.to_string());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process() -> miette::Result<()> {
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!("142", process(input)?);
+        Ok(())
+    }
+}
