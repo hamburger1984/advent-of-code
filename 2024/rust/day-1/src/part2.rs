c0acd1d06638fec5ae0dc69850a3110fe1fbcfc7
Andreas K <hamburger1984@gmail.com>
2024-12-02 14:40:27 +0100

    Day 1 - Part 2 = 22545250

--- 2024/rust/day-1/src/part2.rs
+++ 2024/rust/day-1/src/part2.rs
@@ -1,10 +1,30 @@
 use crate::custom_error::AocError;
+use itertools::Itertools;
 
 #[tracing::instrument]
-pub fn process(
-    _input: &str,
-) -> miette::Result<String, AocError> {
-    todo!("day_1 - part 2");
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let (left, right): (Vec<i32>, Vec<i32>) = input
+        .lines()
+        .map(|l| {
+            l.split_once("   ")
+                .map(|pair| {
+                    (
+                        pair.0.parse::<i32>().unwrap(),
+                        pair.1.parse::<i32>().unwrap(),
+                    )
+                })
+                .unwrap()
+        })
+        .unzip();
+
+    let right_counts = right.into_iter().counts();
+
+    let num_times_occurences = left
+        .into_iter()
+        .map(|l| l as usize * right_counts.get(&l).unwrap_or(&(0usize)))
+        .sum::<usize>();
+
+    return Ok(num_times_occurences.to_string());
 }
 
 #[cfg(test)]
@@ -13,9 +33,13 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "3   4
+4   3
+2   5
+1   3
+3   9
+3   3";
+        assert_eq!("31", process(input)?);
         Ok(())
     }
 }
