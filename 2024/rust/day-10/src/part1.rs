e16a69b13d18140cde5f1e4e48a956dbe53b68e8
Andreas K <hamburger1984@gmail.com>
2024-12-15 17:45:45 +0100

    Day 10 - Part 1 = 756 (still too high)

--- 2024/rust/day-10/src/part1.rs
+++ 2024/rust/day-10/src/part1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
@@ -20,7 +22,7 @@ pub fn process(input: &str) -> miette::Result<String, AocError> {
         for x in 0..width {
             let index = to_index(x, y, width);
             if index == map.len() {
-                panic!("what?!")
+                panic!("nonono");
             }
             if map[index] == 9 {
                 print!("\x1b[31m9\x1b[0m");
@@ -34,68 +36,49 @@ pub fn process(input: &str) -> miette::Result<String, AocError> {
     }
 
     let mut head_values_sum = 0;
+
     for (i, v) in map.iter().enumerate() {
-        if *v == 0 {
-            let (x, y) = from_index(i, width);
-            print!("{} ({},{}) =", i, x, y);
-            let head_value = trail_head_value(width, &map, 0, i, Vec::new());
-            print!("> {:?} ", head_value);
-            println!(
-                " .. {} -> {}",
-                head_values_sum,
-                head_values_sum + head_value.len()
-            );
-            head_values_sum += head_value.len();
+        if *v != 0 {
+            // not a trailhead
+            continue;
         }
+
+        let mut found: HashSet<usize> = HashSet::new();
+        let mut todo: Vec<(usize, u8)> = Vec::new();
+        todo.push((i, 0));
+
+        while todo.len() > 0 {
+            if let Some((curr, level)) = todo.pop() {
+                if map[curr] == 9 {
+                    found.insert(curr);
+                    continue;
+                }
+                let next_level = level + 1;
+
+                if curr > 0 && map[curr - 1] == next_level {
+                    todo.push((curr - 1, next_level));
+                }
+                if (curr % width) < width - 1 && map[curr + 1] == next_level {
+                    todo.push((curr + 1, next_level));
+                }
+                if curr > width && map[curr - width] == next_level {
+                    todo.push((curr - width, next_level));
+                }
+                if curr < map.len() - width && map[curr + width] == next_level {
+                    todo.push((curr + width, next_level));
+                }
+            }
+        }
+
+        println!("{}: {:?}", i, found);
+        head_values_sum += found.len();
     }
+
     Ok(head_values_sum.to_string())
 }
 
 fn to_index(x: usize, y: usize, width: usize) -> usize {
     return x + y * width;
-}
-
-fn from_index(index: usize, width: usize) -> (usize, usize) {
-    return (index % width, index / width);
-}
-
-fn trail_head_value(
-    width: usize,
-    map: &Vec<u8>,
-    curr_level: u8,
-    index: usize,
-    found: Vec<usize>,
-) -> Vec<usize> {
-    if curr_level > 9 {
-        return found;
-    }
-
-    if map[index] == 9 {
-        if found.contains(&index) {
-            return found;
-        }
-        let mut res = found.clone();
-        res.push(index);
-        return res;
-    }
-
-    let next_level = curr_level + 1;
-    let mut result = found.clone();
-
-    if index > 0 && map[index - 1] == next_level {
-        result = trail_head_value(width, map, next_level, index - 1, result);
-    }
-    if index < map.len() - 1 && map[index + 1] == next_level {
-        result = trail_head_value(width, map, next_level, index + 1, result);
-    }
-    if index > width && map[index - width] == next_level {
-        result = trail_head_value(width, map, next_level, index - width, result);
-    }
-    if index < map.len() - width && map[index + width] == next_level {
-        result = trail_head_value(width, map, next_level, index + width, result);
-    }
-
-    return result;
 }
 
 #[cfg(test)]
