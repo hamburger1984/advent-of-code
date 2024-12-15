ffccf0dd8071a4f651a44829982eb98c7b9714c9
Andreas K <hamburger1984@gmail.com>
2024-12-15 17:53:47 +0100

    Day 10 - Part 2 = 1609

--- 2024/rust/day-10/src/part2.rs
+++ 2024/rust/day-10/src/part2.rs
@@ -1,10 +1,82 @@
 use crate::custom_error::AocError;
 
 #[tracing::instrument]
-pub fn process(
-    _input: &str,
-) -> miette::Result<String, AocError> {
-    todo!("day_10 - part 2");
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let width = input.split_once('\n').unwrap().0.len();
+
+    let map = input
+        .lines()
+        .map(|l| {
+            l.chars()
+                .filter(|c| c.is_digit(10))
+                .map(|c| c.to_digit(10).unwrap() as u8)
+        })
+        .flatten()
+        .collect::<Vec<u8>>();
+
+    let height = map.len() / width;
+
+    for y in 0..height {
+        for x in 0..width {
+            let index = to_index(x, y, width);
+            if index == map.len() {
+                panic!("nonono");
+            }
+            if map[index] == 9 {
+                print!("\x1b[31m9\x1b[0m");
+            } else if map[index] == 0 {
+                print!("\x1b[32m0\x1b[0m");
+            } else {
+                print!("{}", map[index]);
+            }
+        }
+        println!("");
+    }
+
+    let mut head_values_sum = 0;
+
+    for (i, v) in map.iter().enumerate() {
+        if *v != 0 {
+            // not a trailhead
+            continue;
+        }
+
+        let mut found: Vec<usize> = Vec::new();
+        let mut todo: Vec<(usize, u8)> = Vec::new();
+        todo.push((i, 0));
+
+        while todo.len() > 0 {
+            if let Some((curr, level)) = todo.pop() {
+                if map[curr] == 9 {
+                    found.push(curr);
+                    continue;
+                }
+                let next_level = level + 1;
+
+                if (curr % width) > 0 && map[curr - 1] == next_level {
+                    todo.push((curr - 1, next_level));
+                }
+                if (curr % width) < width - 1 && map[curr + 1] == next_level {
+                    todo.push((curr + 1, next_level));
+                }
+                if curr > width && map[curr - width] == next_level {
+                    todo.push((curr - width, next_level));
+                }
+                if curr < map.len() - width && map[curr + width] == next_level {
+                    todo.push((curr + width, next_level));
+                }
+            }
+        }
+
+        println!("{}: {:?}", i, found);
+        head_values_sum += found.len();
+    }
+
+    Ok(head_values_sum.to_string())
+}
+
+fn to_index(x: usize, y: usize, width: usize) -> usize {
+    return x + y * width;
 }
 
 #[cfg(test)]
@@ -13,9 +85,16 @@ mod tests {
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("haven't built test yet");
-        let input = "";
-        assert_eq!("", process(input)?);
+        let input = "89010123
+78121874
+87430965
+96549874
+45678903
+32019012
+01329801
+10456732";
+
+        assert_eq!("81", process(input)?);
         Ok(())
     }
 }
