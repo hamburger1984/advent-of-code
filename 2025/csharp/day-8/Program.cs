bf6cc741e4bf60bd80d9da571c2fcb46abdc0b92
Andreas K. <hamburger1984@gmail.com>
2025-12-10 19:07:16 +0100

    Solve 2025 Day 8 Part 2 in C# - Answer: 8135565324
    Part 1: 122430
    Part 2: 8135565324
    
    Day 8 complete! ⭐⭐

--- 2025/csharp/day-8/Program.cs
+++ 2025/csharp/day-8/Program.cs
@@ -42,14 +42,14 @@ static class Solution
         while(closest-- > 0)
         {
             var min = distances.MinBy(d => d.Item3);
-            Console.WriteLine($"Closest pair: {min.Item1}={vectors[min.Item1]} and {min.Item2}={vectors[min.Item2]} with distance {min.Item3}");
+            //Console.WriteLine($"Closest pair: {min.Item1}={vectors[min.Item1]} and {min.Item2}={vectors[min.Item2]} with distance {min.Item3}");
             connected.Add((min.Item1, min.Item2));
 
             distances.Remove(min);
         }
 
         connected.Sort();
-        Console.WriteLine($"Connected pairs: {string.Join(", ", connected)}");
+        //Console.WriteLine($"Connected pairs: {string.Join(", ", connected)}");
 
         var circuits = new List<HashSet<int>>();
 
@@ -80,8 +80,47 @@ static class Solution
 
     public static string Part2(string input)
     {
-        // TODO: Implement part 2
-        throw new NotImplementedException();
+        var vectors = new List<(long, long, long)>();
+
+        foreach(var l in input.Trim().Split('\n'))
+        {
+            var parts = l.Trim().Split(',');
+            vectors.Add((long.Parse(parts[0]), long.Parse(parts[1]), long.Parse(parts[2])));
+        }
+
+        var distances = new List<(int, int, double)>();
+
+        for(var i = 0; i < vectors.Count-1; i++)
+        {
+            for(var j = i + 1; j < vectors.Count; j++)
+            {
+                var (x1, y1, z1) = vectors[i];
+                var (x2, y2, z2) = vectors[j];
+
+                var distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+
+                distances.Add((i, j, distance));
+            }
+        }
+
+        var unconnected = Enumerable.Range(0, vectors.Count).ToHashSet();
+        var connected = new List<(int,int)>();
+
+        while(unconnected.Count > 0)
+        {
+            var min = distances.MinBy(d => d.Item3);
+            //Console.WriteLine($"Closest pair: {min.Item1}={vectors[min.Item1]} and {min.Item2}={vectors[min.Item2]} with distance {min.Item3}");
+            connected.Add((min.Item1, min.Item2));
+
+            distances.Remove(min);
+
+            unconnected.Remove(min.Item1);
+            unconnected.Remove(min.Item2);
+        }
+
+        var lastConnection = connected.Last();
+
+        return (vectors[lastConnection.Item1].Item1 * vectors[lastConnection.Item2].Item1).ToString();
     }
 }
 
@@ -92,7 +131,7 @@ static class Tests
     public static void RunTests()
     {
         TestPart1();
-        //TestPart2();
+        TestPart2();
         Console.WriteLine("All tests passed!");
     }
 
@@ -129,8 +168,27 @@ static class Tests
 
     static void TestPart2()
     {
-        const string input = @"TODO: Add example input";
-        const string expected = "TODO";
+        const string input = @"162,817,812
+57,618,57
+906,360,560
+592,479,940
+352,342,300
+466,668,158
+542,29,236
+431,825,988
+739,650,466
+52,470,668
+216,146,977
+819,987,18
+117,168,530
+805,96,715
+346,949,466
+970,615,88
+941,993,340
+862,61,35
+984,92,344
+425,690,689";
+        const string expected = "25272";
 
         var result = Solution.Part2(input);
         if (result != expected)
